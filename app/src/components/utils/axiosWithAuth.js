3f60bcbc9f0346e23ea819e8fc20dedd2ccc3222
tca88 <trishnaamin@gmail.com>
2019-05-10 13:44:46 -0400

    Add link to login page if user isn't logged in and tries to access the jokes page.

--- app/src/components/utils/axiosWithAuth.js
+++ app/src/components/utils/axiosWithAuth.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 axios.defaults.baseURL = "http://localhost:3300/";
 
@@ -18,7 +19,12 @@ export default function(Component) {
   return class Authenticated extends React.Component {
     render() {
       const token = localStorage.getItem("jwt");
-      const notLoggedIn = <h3>Please login to see users</h3>;
+      const notLoggedIn = (
+        <div>
+          <h3>Please login to see users</h3>
+          <Link to="/">Login</Link>
+        </div>
+      );
 
       return <>{token ? <Component {...this.props} /> : notLoggedIn}</>;
     }
